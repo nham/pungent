6ce16a04ded5774274010559f42c0a592f057b16
Nick Hamann <hamann.nick@gmail.com>
2011-04-06 23:41:42 -0500

    Added a dummy config file. Probably need a better location for this than lib/ eventually...

---
+++ lib/config.php
@@ -0,0 +1,18 @@
+<?php
+
+class Config {
+  public static $site_url = "";
+
+  //db
+  public static $db_host = "";
+  public static $db_name = "";
+  public static $db_user = "";
+  public static $db_pass = "";
+
+  //auth
+  public static $f_app_id = "";
+  public static $f_app_secret = "";
+
+  public static $t_consumer_key = "";
+  public static $t_consumer_secret = "";
+}
